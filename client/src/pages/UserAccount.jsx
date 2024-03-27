8d4d2f7c2a87c65e1069b74cb234075bebd0126e
mcoca <mcoca4280@gmail.com>
2024-03-27 12:21:46 -0400

    added profile,lists, rec sections

--- client/src/pages/UserAccount.jsx
+++ client/src/pages/UserAccount.jsx
@@ -2,7 +2,18 @@ import React from "react"
 
 const UserAccount = () => {
     return (
-        <div>User Account</div>
+        <div>
+            <h1>User Account</h1>
+            <div id="profile">
+                <h2>Hello, [FETCH USER]!</h2>
+            </div>
+            <div id="list-container">
+                <div id="list">sample list</div>
+            </div>
+            <div id="recommender">
+                Give Me a ReelMatch! [(BUTTON)Take Our Quiz!]
+            </div>
+        </div>
     )
 }
 
