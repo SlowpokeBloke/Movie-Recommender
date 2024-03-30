29fef1bb33f9af9ed8f6dfe24ae9e30275e67824
leslyvv <vargaslesly64@gmail.com>
2024-03-29 22:07:45 -0400

    adding partial quiz files

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,12 @@
 import express from "express";
 import mysql from "mysql";
-
+import cors from "cors";
 const app = express();
 
 const db = mysql.createConnection({
     host: "localhost", 
     user: "root",
-    password: "Bella8903_",
+    password: "lele123!",
     database: "movie_recommender"
 });
 
@@ -18,10 +18,72 @@ db.connect((err) => {
     console.log("Connected to database!");
 });
 
-app.use(express.json())
+app.use(express.json());
+app.use(cors())
 
-app.get("/", (req, res) => {
-    res.json("Hello, this is the backend");
+app.get("/", (req, res)=>{
+    console.log("Received request for /")
+    res.json("hello this is the backend")
+})
+app.get("/movie", (req,res)=>{
+    const q = "SHOW TABLES;"
+    db.query(q,(err,data)=>{
+        if(err) return res.json(err)
+        return res.json(data)
+    })
+
+})
+app.get("/movie_table", (req, res) => {
+    console.log("Handling /movie request");
+    const q = "SHOW TABLES;";
+    db.query(q, (err, data) => {
+        if (err) {
+            console.error("Database query error:", err);
+            return res.json(err);
+        }
+        console.log("Database query success:", data);
+        return res.json(data);
+    });
+});
+
+app.get("/actor", (req, res) => {
+    console.log("Handling /movie request");
+    const q = "SELECT * FROM actor;";
+    db.query(q, (err, data) => {
+        if (err) {
+            console.error("Database query error:", err);
+            return res.json(err);
+        }
+        console.log("Database query success:", data);
+        return res.json(data);
+    });
+});
+
+app.get("/genre", (req, res) => {
+    console.log("Handling /movie request");
+    const q = "SELECT * FROM genre;";
+    db.query(q, (err, data) => {
+        if (err) {
+            console.error("Database query error:", err);
+            return res.json(err);
+        }
+        console.log("Database query success:", data);
+        return res.json(data);
+    });
+});
+
+app.post("/genre", (req, res) => {
+    console.log("Handling /movie request");
+    const q = "INSERT INTO genre (`genre_id`, `genre_name`) VALUES(?);";
+    const values = [12, 'Thriller']
+    db.query(q,[values],(err,data)=>{
+        if (err) {
+            console.error("Database query error:", err);
+            return res.json(err);
+        }
+        console.log("Database query success:", data);
+        return res.json(data);
+    })
 });
 
 app.listen(8800, () => {
