2f9a2c7397d3fb8b6fd81eeec881606ab07a8411
mcoca <mcoca4280@gmail.com>
2024-04-01 18:10:56 -0400

    modified global container style with round border created account_landing.css to set basic style for UserAccount.jsx

--- client/src/pages/UserAccount.jsx
+++ client/src/pages/UserAccount.jsx
@@ -1,19 +1,31 @@
 import React from "react"
+import "./account_landing.css";
 import { Link, NavLink } from 'react-router-dom';
 
 const UserAccount = () => {
     return (
-        <div name="profile-container" class="container">
+        <div name="account-container" class="page-container">
             <h1>User Account</h1>
-            <div id="profile">
-                <h2>Hello, [FETCH USER]!</h2>
-            </div>
-            <div id="list-container">
-                <div id="list">sample list</div>
-            </div>
-            <div id="recommender">
-                Give Me a ReelMatch!
-                <Link to ="/Quiz"><button>Take Our Quiz!</button></Link>
+            <div id="profile-container" class="container">
+                <section id="profile">
+                    <h2>Hello, [FETCH USER]!</h2>
+                </section>
+                <section id="acctmovie-container">
+                    <section id="list-container">
+                        <div id="favorite-list" class="list">
+                            <h3>Your Favorite Movies</h3>
+                            <p>No movies favorited!</p>
+                        </div>
+                        <div id="towatch-list" class="list">
+                            <h3>Your Movies To Watch</h3>
+                            <p>No movies planned to watch!</p>
+                        </div>
+                    </section>
+                    <section id="recommender">
+                        Give Me a ReelMatch!
+                        <Link to ="/Quiz"><button>Take Our Quiz!</button></Link>
+                    </section>
+                </section>
             </div>
         </div>
     )
